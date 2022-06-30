f5400d0f058e7ffa50b3c288bdcdd94b2dc2e654
Prasad Londhe <prasgl@gmail.com>
2022-06-30 21:08:16 +0530

    parse argument format verification complete

--- splunk-like-time-parser.js
+++ splunk-like-time-parser.js
@@ -1,7 +1,82 @@
+const modifiers = ['now()'];
+const snap = '@';
+const snapUnits = [
+    's',
+    'm',
+    'h',
+    'd',
+    'mon',
+    'y'
+];
+const operators = ['+', '-'];
+
+const getOffsets = (offsets) => {
+    const set = [];
+    let copy = offsets;
+    while (copy) {
+        let operator, offsetPeriod, offsetUnit;
+        operator = operators.find(o => copy.substring(0, o.length) === o);
+        if (operator) {
+            copy = copy.substring(operator.length);
+        } else {
+            throw new Error(`invalid operator ${copy[0 - o.length]}`);
+        }
+
+        const period = copy.match(/^\d+/);
+        if (period) {
+            offsetPeriod = period[0];
+            copy = copy.substring(offsetPeriod.length);
+        } else {
+            throw new Error(`invalid offset period`);
+        }
+
+        const unit = copy.match(/^mon|^[smhdy]/);
+        if (unit) {
+            offsetUnit = unit[0];
+            copy = copy.substring(offsetUnit.length);
+        } else {
+            throw new Error(`invalid offset unit`);
+        }
+
+        set.push({operator, offsetPeriod, offsetUnit});
+    }
+    return {set}
+}
+
 exports.parse = (expr) => {
     if (typeof expr !== 'string') {
-        throw new Error("parse: A single argument is expected and should be a string");
+        throw new TypeError("parse: A single argument is expected and should be a string");
     }
-    return {};
+    
+    const expression = expr.replace(/\s/g, '');
+
+    const modifier = modifiers.find(m => expression.startsWith(m));
+
+    if (!modifier) {
+        throw new Error(`parse: the argument must start with ${modifiers}`);
+    }
+    
+    let snapUnit;
+    const snapPosition = expression.indexOf(snap); // TODO last index of?
+    if (snapPosition !== -1) {
+        const unit = expression.substring(snapPosition + 1);
+        snapUnit = snapUnits.find(u => u === unit);
+
+        if (!snapUnit) {
+            throw new Error(`parse: Can only snap to ${snapUnits}`);
+        }
+
+        if (snapUnit && !expression.endsWith(`${snap}${snapUnit}`)) {
+            throw new Error(`parse: the argument must end with ${snap} and unit if specified`);
+        }
+    }
+
+    let offsets = expression.substring(modifier.length);
+    if (snapUnit) {
+            offsets = offsets.substring(0, offsets.length - `${snap}${snapUnit}`.length);
+    }
+    const offsetsArray = getOffsets(offsets);
+
+    return new Date();
 }
 
--- test/splunk-like-time-parser-spec.js
+++ test/splunk-like-time-parser-spec.js
@@ -5,16 +5,44 @@ const main = require('../splunk-like-time-parser');
 describe('Splunk-like Time Parser', () => {
     it('parse requires a string argument and returns a (date) object', () => {
         var argsTest = main.parse;
-        // invoke without args
-        expect(argsTest).to.throw(Error);
 
-        // invoke with non-string arg type
-        expect(() => argsTest(123)).to.throw(Error);
+        // if the arg is a string, no TypeError should be thrown
+        expect(argsTest).to.throw(TypeError); // no arg specified
+        expect(() => argsTest(123)).to.throw(TypeError); // incorrect arg type
+        expect(() => argsTest('test')).to.not.throw(TypeError);
 
-        // if the arg is a string, no error should be thrown
-        expect(() => argsTest('test')).to.not.throw(Error);
+        // argument should start with modifier
+        expect(() => argsTest('test')).to.throw(Error);
+        expect(() => argsTest('now()')).to.not.throw(Error);
 
-        // it should return an object 
-        expect(argsTest('test')).to.be.an('object');
+        // any spaces embedded in the argument should be ignored
+        expect(() => argsTest('now ( ) + 1 d @ mon')).to.not.throw(Error);
+
+        // if snap (@) is specified, it should be with snap-unit and at the end of the argument
+        expect(() => argsTest('now()@+1m')).to.throw(Error);
+        expect(() => argsTest('now()@')).to.throw(Error);
+        expect(() => argsTest('now()@x')).to.throw(Error);
+        expect(() => argsTest('now()@mon')).to.not.throw(Error);
+
+        // offset/s if specified, should start with an operator (+ or -)
+        expect(() => argsTest('now()1m')).to.throw(Error);
+        expect(() => argsTest('now()+1m')).to.not.throw(Error);
+        expect(() => argsTest('now()-1m')).to.not.throw(Error);
+
+        // offset should contain numeric period after operator
+        expect(() => argsTest('now()+xm')).to.throw(Error);
+        expect(() => argsTest('now()+5mon')).to.not.throw(Error);
+
+        // offset should support periods s, m, h, d, mon, y
+        expect(() => argsTest('now()+5s')).to.not.throw(Error);
+        expect(() => argsTest('now()+5m')).to.not.throw(Error);
+        expect(() => argsTest('now()+5h')).to.not.throw(Error);
+        expect(() => argsTest('now()+5d')).to.not.throw(Error);
+        expect(() => argsTest('now()+5mon')).to.not.throw(Error);
+        expect(() => argsTest('now()+5y')).to.not.throw(Error);
+        expect(() => argsTest('now()+5z')).to.throw(Error);
+
+        // snap and offset specified, should not throw error
+        expect(() => argsTest('now()+5y@m')).to.not.throw(Error);
     });
 });
